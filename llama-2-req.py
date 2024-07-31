3c8aa6cf2a1c9c165c238815141792d643182edf
Ubuntu <ubuntu@ip-172-31-45-123.ec2.internal>
2024-07-31 16:54:40 +0000

    dbg

---
+++ llama-2-req.py
@@ -0,0 +1,15 @@
+import openai
+
+# Set OpenAI's API key and API base to use vLLM's API server.
+openai.api_key = "EMPTY"  # Not needed since we're using a local server
+openai.api_base = "http://localhost:8000/v1"
+
+response = openai.ChatCompletion.create(
+    model="NousResearch/Llama-2-7b-chat-hf",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What is Congo Square's significance?"},
+    ]
+)
+
+print(response.choices[0].message["content"])
