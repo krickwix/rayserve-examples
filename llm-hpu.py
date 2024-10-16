34e41530d563f7a988d3940d45cc4d5358af7a50
Fabien Andrieux <krickwix@gmail.com>
2024-10-16 09:58:22 +0000

    HPU

--- llm-hpu.py
+++ llm-hpu.py
@@ -127,10 +127,12 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     else:
         accelerator = "HPU"
     # get environment variable MODEL_NAME
-    model_name = os.getenv("MODEL_NAME")
     parsed_args = parse_vllm_args(cli_args)
     engine_args = AsyncEngineArgs.from_cli_args(parsed_args)
     engine_args.worker_use_ray = True
+    model_name = os.getenv("HF_MODEL_NAME")
+    if model_name is None:
+        model_name = "meta-llama/Meta-Llama-3-70B-Instruct"
     # engine_args.model = "meta-llama/Meta-Llama-3-70B-Instruct"
     # engine_args.tokenizer = "meta-llama/Meta-Llama-3-70B-Instruct"
     engine_args.model = model_name
