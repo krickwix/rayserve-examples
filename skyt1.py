3b9087b9296d27f64d7e243b19681f06f0c754cd
Fabien Andrieux <krickwix@gmail.com>
2025-01-15 11:12:26 +0000

    sky

--- skyt1.py
+++ skyt1.py
@@ -172,9 +172,12 @@ def build_app(model_name: str, tensor_parallel_size: int) -> serve.Application:
         served_model_name=model_name,
         tensor_parallel_size=tensor_parallel_size,
         worker_use_ray=True,
-        rope_scaling={
-            "type": "dynamic",
-            "factor": 2.0
+        rope_scaling = {
+            "rope_type": "yarn",
+            "factor": 4.0,
+            "original_max_position_embeddings": 32768,
+            "beta_fast": 32,
+            "beta_slow": 1
         }
     )
 
