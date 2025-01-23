cc5cf01188c253655c1841ba2208f13855aa3c2b
Fabien Andrieux <krickwix@gmail.com>
2025-01-23 00:21:08 +0000

    mistrallarge

--- mistrallarge.py
+++ mistrallarge.py
@@ -170,13 +170,13 @@ def build_app(model_name: str, tensor_parallel_size: int) -> serve.Application:
         served_model_name=model_name,
         tensor_parallel_size=tensor_parallel_size,
         worker_use_ray=True,
-        rope_scaling = {
-            "rope_type": "yarn",
-            "factor": 4.0,
-            "original_max_position_embeddings": 32768,
-            "beta_fast": 32,
-            "beta_slow": 1
-        }
+        # rope_scaling = {
+        #     "rope_type": "yarn",
+        #     "factor": 4.0,
+        #     "original_max_position_embeddings": 32768,
+        #     "beta_fast": 32,
+        #     "beta_slow": 1
+        # }
     )
 
     logger.info(f"Tensor parallelism = {tensor_parallel_size}")
