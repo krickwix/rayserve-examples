f1d083aaf124a6fc75c957dd246e38eab7742527
Fabien Andrieux <krickwix@gmail.com>
2025-01-23 10:19:20 +0000

    qwen

--- qwen2.py
+++ qwen2.py
@@ -198,6 +198,7 @@ def build_app(model_name: str, tensor_parallel_size: int) -> serve.Application:
 
 # Initialize the deployment
 deployment = build_app(
-    model_name="Qwen/Qwen2.5-72B-Instruct", 
+    # model_name="Qwen/Qwen2.5-72B-Instruct", 
+    model_name="Qwen/Qwen2.5-0.5B-Instruct",
     tensor_parallel_size=8
 )
