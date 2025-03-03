e346f9f3c27e270c971e0b157454710f791bebf6
Fabien Andrieux <krickwix@gmail.com>
2025-03-03 16:37:35 +0000

    ngram tuning

--- llm-spec.py
+++ llm-spec.py
@@ -205,8 +205,8 @@ def build_app(model_name: str, tensor_parallel_size: int) -> serve.Application:
         "distributed_executor_backend": "ray",
         "trust_remote_code": True,
         "speculative_model": "[ngram]",
-        "num_speculative_tokens": 5,
-        "ngram_prompt_lookup_max": 4,
+        "num_speculative_tokens": 10,
+        "ngram_prompt_lookup_max": 5,
         "use_v2_block_manager": True,
     }
     # Merge base arguments with additional arguments
